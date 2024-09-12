d00eb5f1b1c70e9ed8e956bc7581444a53734d27
mrmik78 <m.reutsky@mail.ru>
2024-09-12 22:29:32 +0300

    initial commit

---
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class OrderCardTest {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+
+    }
+}
