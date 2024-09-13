5e999a9d88461648ca7676f8a90f31771db16c6f
mrmik78 <m.reutsky@mail.ru>
2024-09-13 20:33:50 +0300

    update OrderCardTest2

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -12,8 +12,8 @@ class OrderCardTest {
     void shouldTest() {
         open("http://localhost:9999");
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Иванов");
-        form.$("[data-test-id=phone] input").setValue("+792700000000");
+        form.$("[data-test-id=name] input").setValue("Петров");
+        form.$("[data-test-id=phone] input").setValue("+79110000000");
         form.$("[data-test-id=agreement]").click();
         $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
         $(".button__text").click();
