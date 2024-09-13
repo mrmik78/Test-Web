4a8d58a80f1f25a7166157a73ecc495bb343efe0
mrmik78 <m.reutsky@mail.ru>
2024-09-13 20:26:08 +0300

    add gradle.yml&update OrderCardTest

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -1,12 +1,21 @@
 package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class OrderCardTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иванов");
+        form.$("[data-test-id=phone] input").setValue("+792700000000");
+        form.$("[data-test-id=agreement]").click();
+        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".button__text").click();
     }
 }
