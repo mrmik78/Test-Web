7d03d390282b130017719c84d8a5d672567aaa8e
mrmik78 <m.reutsky@mail.ru>
2024-09-14 20:16:51 +0300

    update OrderCardTest3

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -2,7 +2,6 @@ package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -12,10 +11,10 @@ class OrderCardTest {
     void shouldTest() {
         open("http://localhost:9999");
         SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Петров");
+        form.$("[data-test-id=name] input").setValue("Петров Василий");
         form.$("[data-test-id=phone] input").setValue("+79110000000");
         form.$("[data-test-id=agreement]").click();
-        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
         $(".button__text").click();
+        $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
